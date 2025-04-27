80627d7ef439372e2eb84c31b4afb7f7e8a5a5e6
yonguk <kimyu9827@naver.com>
2025-04-27 16:55:37 +0900

    feature: 질문 등록 및 값 검증

--- src/main/java/com/webtoys/GPTI/Question.java
+++ src/main/java/com/webtoys/GPTI/Question.java
@@ -4,8 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/com/webtoys/GPTI/service.java
+++ src/main/java/com/webtoys/GPTI/service.java
@@ -1,15 +1,57 @@
 package com.webtoys.GPTI;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+/**
+
+ */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class service {
     private QuestionRepository questionRepository;
 
     // 질문 가져오기
+    public List<Question> getAllQuestions(){
+        return questionRepository.findAll();
+    }
 
-    // 질문 등록하기dd
-    
+    // 질문 등록하기
+    public void addQuestion(String string, String stringType){
+        questionRepository.save(
+                Question.builder()
+                        .question(questionStringCheck(string))
+                        .type(stringTypeToInteger(stringType))
+                        .build()
+        );
+    }
+
+    private String questionStringCheck(String string){
+        if (string.isBlank()){
+            log.error("question sentence is blank");
+            throw new IllegalArgumentException("question sentence is blank");
+        }
+        return string;
+    }
+
+    private Integer stringTypeToInteger(String stringType){
+        if(stringType.equalsIgnoreCase("I") || stringType.equalsIgnoreCase("E")){
+            return 1;
+        } else if(stringType.equalsIgnoreCase("S") || stringType.equalsIgnoreCase("N")){
+            return 2;
+        } else if(stringType.equalsIgnoreCase("T") || stringType.equalsIgnoreCase("F")){
+            return 3;
+        } else if(stringType.equalsIgnoreCase("J") || stringType.equalsIgnoreCase("P")){
+            return 4;
+        } else {
+            return 0;
+        }
+    }
+
+
 }
