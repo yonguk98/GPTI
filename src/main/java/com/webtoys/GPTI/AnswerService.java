0b4b5974a3a6d825c52ef0ab5afed184bb30933b
yongwook <kimyu9827@naver.com>
2025-05-02 20:31:15 +0900

    fix : 사용하지 않는 클래스 import 삭제

--- src/main/java/com/webtoys/GPTI/AnswerService.java
+++ src/main/java/com/webtoys/GPTI/AnswerService.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
--- src/main/java/com/webtoys/GPTI/controller.java
+++ src/main/java/com/webtoys/GPTI/controller.java
@@ -2,7 +2,6 @@ package com.webtoys.GPTI;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
